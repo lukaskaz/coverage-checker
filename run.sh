d77e7d40eda24c04cdbe9a33fe43aca375d9f914
lukaskaz <lukask84@o2.pl>
2024-07-04 15:22:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+BUILDDIR=$1
 SCRIPTTORUN=check-coverage.sh
 RETCODE=1
 
-if [ -n "$SCRIPTTORUN" -a -f "$SCRIPTTORUN" ]; then
-	./$SCRIPTTORUN
+if [ -n "$BUILDDIR" -a -d "$BUILDDIR" ]; then
+	./$SCRIPTTORUN $BUILDDIR
 	RETCODE=$?
 else
 	echo "Script to run: '$SCRIPTTORUN' is invalid"
 fi
 
 exit $RETCODE
-
